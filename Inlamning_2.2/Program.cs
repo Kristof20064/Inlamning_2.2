3d6fddb8710f6b9b446c5edbfe689f0448581370
krbu06001 <krbu06001@E-PW05CGQY>
2024-10-03 13:55:08 +0200

    Add project files.

---
+++ Inlamning_2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+
+namespace Inlamning_2._2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin? (Svara i meter)");
+            int Elin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur långt hoppade Alma? (Svara i meter)");
+            int Alma = int.Parse(Console.ReadLine());
+
+            int skillnad = Elin - Alma;
+
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre.");
+        }
+    }
+}
